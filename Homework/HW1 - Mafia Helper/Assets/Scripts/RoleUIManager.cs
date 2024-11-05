af4c57032ca391fdf29dc45825ca2672cd64ae8b
daniyeg <daniyeg2@gmail.com>
2024-11-05 09:20:10 +0330

    fix role screen dynamic texts appearing backwards

--- Homework/HW1 - Mafia Helper/Assets/Scripts/RoleUIManager.cs
+++ Homework/HW1 - Mafia Helper/Assets/Scripts/RoleUIManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using ArabicSupport;
 
 public class RoleUIManager : MonoBehaviour
 {
@@ -7,12 +8,15 @@ public class RoleUIManager : MonoBehaviour
     public GameObject teamText;
     public GameObject roleImage;
 
+    public bool showTashkeel = true;
+    public bool useHinduNumbers = true;
+
     void Start()
     {
         var role = Deck.Instance.Deal();
 
-        nameText.GetComponent<Text>().text = role.Name;
-        teamText.GetComponent<Text>().text = role.Team;
+        nameText.GetComponent<Text>().text = ArabicFixer.Fix(role.Name, showTashkeel, useHinduNumbers);
+        teamText.GetComponent<Text>().text = ArabicFixer.Fix(role.Team, showTashkeel, useHinduNumbers);
         roleImage.GetComponent<RawImage>().texture = role.Texture;
     }
 
