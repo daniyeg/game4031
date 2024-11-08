3048c9679f8bf553fc6ff3f663b81a9ecef11a87
daniyeg <daniyeg2@gmail.com>
2024-11-08 23:42:25 +0330

    add pictures to roles

--- Homework/HW1 - Mafia Helper/Assets/Scripts/Role.cs
+++ Homework/HW1 - Mafia Helper/Assets/Scripts/Role.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,14 +8,16 @@ public class Role
     public string Name;
     public string Team;
     public string Description;
-    public Texture Texture;
+    public Texture2D Texture;
 
     public Role(string name, string team, string description, string texturePath)
     {
         Name = name;
         Team = team;
         Description = description;
-        Texture = Resources.Load<Texture>(texturePath);
+        byte[] fileData = File.ReadAllBytes(texturePath);
+        Texture = new Texture2D(2, 2);
+        Texture.LoadImage(fileData);
         if (Texture == null)
         {
             Debug.LogError($"Failed to load texture {texturePath}.");
@@ -23,15 +26,15 @@ public class Role
 
     public static Dictionary<string, Func<Role>> RolesDictionary = new Dictionary<string, Func<Role>>()
     {
-        ["Godfather"] = () => new Role("پدرخوانده", "مافیا", "", ""),
-        ["Mafioso"] = () => new Role("مافیای ساده", "مافیا", "", ""),
-        ["Proffesional"] = () => new Role("حرفه‌ای", "مافیا", "", ""),
-        ["Murderer"] = () => new Role("قاتل", "مافیا", "", ""),
+        ["Godfather"] = () => new Role("پدرخوانده", "مافیا", "", "Assets/Sprites/godfather.jpg"),
+        ["Mafioso"] = () => new Role("مافیای ساده", "مافیا", "", "Assets/Sprites/mafia.jpg"),
+        ["Proffesional"] = () => new Role("حرفه ای", "مافیا", "", "Assets/Sprites/mafia.jpg"),
+        ["Murderer"] = () => new Role("قاتل", "مافیا", "", "Assets/Sprites/mafia.jpg"),
 
-        ["Detective"] = () => new Role("کارآگاه", "شهروند", "", ""),
-        ["Doctor"] = () => new Role("دکتر", "شهروند", "", ""),
-        ["Sniper"] = () => new Role("اسنایپر", "شهروند", "", ""),
-        ["Civilian"] = () => new Role("شهروند ساده", "شهروند", "", ""),
-        ["Mayor"] = () => new Role("شهردار", "شهروند", "", "")
+        ["Detective"] = () => new Role("کارآگاه", "شهروند", "", "Assets/Sprites/detective.jpg"),
+        ["Doctor"] = () => new Role("دکتر", "شهروند", "", "Assets/Sprites/doctor.jpg"),
+        ["Sniper"] = () => new Role("اسنایپر", "شهروند", "", "Assets/Sprites/sniper.jpg"),
+        ["Civilian"] = () => new Role("شهروند ساده", "شهروند", "", "Assets/Sprites/civilian.jpg"),
+        ["Mayor"] = () => new Role("شهردار", "شهروند", "", "Assets/Sprites/mayor.jpg")
     };
 }
